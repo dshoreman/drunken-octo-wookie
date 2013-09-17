fe980bbc4386a04143cf0cfc0988a2b3f5a63701
Dave <codeM0nK3Y@me.com>
2013-09-18 00:31:09 +0100

    Add Ajax error handler [Fixes #4]
    When the session with Google API has timed out, Ajax requests silently
    redirect to the login page on Google. This commit fixes that by
    intercepting all Ajax errors and printing an error panel instead.

--- public/assets/js/custom.js
+++ public/assets/js/custom.js
@@ -2,10 +2,16 @@ debug('Debug mode activated!');
 
 $(document).ready(function() {
 
+	/**
+	 * Toggling sidebar for extra-small viewports
+	 */
 	$('[data-toggle=offcanvas]').click(function() {
 		$('.row-offcanvas').toggleClass('active');
 	});
 
+	/**
+	 * Handle internal links for a smoother experience
+	 */
 	$('body').on('click', 'a[href*="/channels/"], a[href*="/playlists/"]', function(e) {
 
 		e.preventDefault();
@@ -17,6 +23,9 @@ $(document).ready(function() {
 		});
 	});
 
+	/**
+	 * Initialise any tab groups
+	 */
 	$('body').on('click', '#channel_tabs a', function(e) {
 
 		e.preventDefault();
@@ -25,3 +34,18 @@ $(document).ready(function() {
 	});
 
 });
+
+/**
+ * Ajax Error handler
+ */
+$(document).ajaxError(function(e, jqxhr, settings, error) {
+
+	var title = '<h3 class="panel-title">Oh noes!</h3>',
+		heading = '<div class="panel-heading">'+title+'</div>',
+		body = '<div class="panel-body">'+error+'</div>',
+		error = '<div class="panel panel-danger">'+heading+body+'</div>';
+
+	$('.main-content-panel').html(error);
+});
+
+debug('All scripts complete.');
