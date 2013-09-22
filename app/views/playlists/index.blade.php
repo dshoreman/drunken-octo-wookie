ab0d537ea801ec6abe89c22dd9290337c2ee1120
Dave <codeM0nK3Y@me.com>
2013-09-22 18:00:08 +0100

    Add alternative views to playlist items

--- app/views/playlists/index.blade.php
+++ app/views/playlists/index.blade.php
@@ -10,24 +10,55 @@
 	</div>
 </div>
 
-@foreach ($items as $item)
-	<div class="panel panel-default video">
-		<div class="panel-heading">
-			<h3 class="panel-title">{{ $item['snippet']['title'] }}</h3>
-		</div>
-		<div class="panel-body">
-			<div class="media">
-				<a href="{{ route('player', $item['snippet']['resourceId']['videoId']) }}" class="pull-left">
-					<img src="{{ $item['snippet']['thumbnails']['default']['url'] }}" />
-				</a>
-				<div class="media-body">
-					<p class="description">{{ Str::limit($item['snippet']['description'], 150, '&hellip;') }}</p>
-					<small>Published by {{ $item['snippet']['channelTitle'] }} on {{ $item['snippet']['publishedAt'] }}</small>
+<div id="playlist-items">
+	<div class="content">
+		<div class="row view-minimal hidden">
+			@foreach ($items as $item)
+				<div class="col-sm-6 col-md-3">
+					<a href="{{ route('player', $item['snippet']['resourceId']['videoId']) }}" class="thumbnail">
+						<img src="{{ $item['snippet']['thumbnails']['default']['url'] }}" />
+					</a>
 				</div>
-			</div>
+			@endforeach
+		</div>
+
+		<div class="row view-small hidden">
+			@foreach ($items as $item)
+				<div class="col-sm-6 col-md-3">
+					<div class="thumbnail">
+						<a href="{{ route('player', $item['snippet']['resourceId']['videoId']) }}" class="thumbnail">
+							<img src="{{ $item['snippet']['thumbnails']['default']['url'] }}" />
+						</a>
+						<div class="caption">
+							<h5>{{ $item['snippet']['title'] }}</h5>
+						</div>
+					</div>
+				</div>
+			@endforeach
+		</div>
+
+		<div class="view-list">
+			@foreach ($items as $item)
+				<div class="panel panel-default video">
+					<div class="panel-heading">
+						<h3 class="panel-title">{{ $item['snippet']['title'] }}</h3>
+					</div>
+					<div class="panel-body">
+						<div class="media">
+							<a href="{{ route('player', $item['snippet']['resourceId']['videoId']) }}" class="pull-left">
+								<img src="{{ $item['snippet']['thumbnails']['default']['url'] }}" />
+							</a>
+							<div class="media-body">
+								<p class="description">{{ Str::limit($item['snippet']['description'], 150, '&hellip;') }}</p>
+								<small>Published by {{ $item['snippet']['channelTitle'] }} on {{ $item['snippet']['publishedAt'] }}</small>
+							</div>
+						</div>
+					</div>
+				</div>
+			@endforeach
 		</div>
 	</div>
-@endforeach
+</div>
 
 @if (isset($paging['prev']) || isset($paging['next']))
 <ul class="pager">
@@ -39,3 +70,7 @@
 	</li>
 </ul>
 @endif
+
+<script type="text/javascript">
+$('#playlist-items .content').multiview();
+</script>
